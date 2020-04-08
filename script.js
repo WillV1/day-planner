673c1d10b381ea7d69c873986858f3489667b77e
WillV1 <wmvanhook@gmail.com>
2020-04-08 10:53:54 -0400

    Committing current progress on project

--- script.js
+++ script.js
@@ -2,61 +2,67 @@
 
 $(document).ready(function () {
   var date = moment().format('dddd MMMM Do');
-  console.log(date);
+
   $('#currentDay').text(date);
 
   //Create code that adds in business hours (9am - 5pm) into file for adding in hourly events to the left of the 
   //event text box
 
-  moment().hour(i)
+  //  moment().hour(i)
+  //  var hour = ('#9');
 
-  for (var i = 9; i<=17; i++) {
-    console.log(moment().hour(i).locale('en').format('hA'));
-    if (time === ''){
-      $(time).text(moment().hour(i).locale('en').format('hA'))
-    } else {
-      ''
-    }
-    }
+  //for (var i = 9; i <= 17; i++) {
+  //    console.log(moment().hour(i).locale('en').format('hA'));
+  //    if ('#9' === '') {
+  //      $(hour).html(moment().hour(i).locale('en').format('hA'))
+  //    } else {
+  //      ''
+  //    }
+  //  }
 
-//Set styling so that business hours that have passed are greyed out;  if / else for time component 
-//Set styling so that the current time/hour is a red color;
- //Set styling so that hours that have not yet occured are in green 
-var time = $('.time-block');
+  //Set styling so that business hours that have passed are greyed out;  if / else for time component 
+  //Set styling so that the current time/hour is a red color;
+  //Set styling so that hours that have not yet occured are in green 
+  var time = $('.time-block');
 
- function currentTime (){
-  var currentTime = moment().hours();
-  console.log(currentTime);
-  time.each(function (){
-  var hour = parseInt($(this).attr('id'))
-  console.log($(this))
-    if (hour === currentTime){
-  $('.time-block > textarea').attr('class','present col-sm-10')
-  } else if(currentTime > hour){
-    time.attr('class', 'past')
-  } else {
-    time.attr('class', 'future')
+  function currentTime() {
+    var current = moment().hours()
+
+    console.log(current);
+
+    time.each(function () {
+      var hour = parseInt($(this).attr('id'))
+
+      console.log(hour);
+
+      if (hour === current) {
+        $('.time-block > textarea').attr('class', 'present col-sm-10')
+      } else if (current > hour) {
+        $('.time-block > textarea').attr('class', 'past col-sm-10')
+      } else {
+        $('.time-block > textarea').attr('class', 'future col-sm-10')
+      }
+    })
   }
- })}
   currentTime()
- 
-//Set up local storage to store persistent changes in events by timeblock
 
-var button = $('.saveBtn')
+  //Set up local storage to store persistent changes in events by timeblock
 
-button.on('click', function(event) {
-  event.preventDefault();
-  var text = $(this).siblings('.col-sm-10').val();
-  console.log(text);
-  var parent = $(this).parent().attr('id');
-  localStorage.setItem(parent, JSON.stringify(text));
+  var button = $('.saveBtn')
 
-console.log(parent);
-  
-})
-//Use styling to transform pointer into a disk icon at the time of click event (saving of event)
-$("#9 .col-sm-10").val(localstorage.getItem("9"))
-localStorage.getItem(JSON.parse(parent))
+  button.on('click', function (event) {
+    event.preventDefault();
+    var text = $(this).siblings('.col-sm-10').val();
+    var parent = $(this).parent().attr('id');
+    localStorage.setItem(parent, JSON.stringify(text));
+
+    var toDo = (JSON.parse(localStorage.getItem(parent)));
+    $('#9 textarea').text(toDo);
+    console.log(parent);
+
+  })
+  //Use styling to transform pointer into a disk icon at the time of click event (saving of event)
+
 
 
 });
