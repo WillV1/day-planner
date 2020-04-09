6f5007b0f2fb9ac21ada325963325a6690d4db8f
WillV1 <wmvanhook@gmail.com>
2020-04-09 06:24:12 -0400

    Commiting updated updated pages to GitHub

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var button = $('.saveBtn')
+
 //Add current date at the top of the planner 
 
 $(document).ready(function () {
@@ -8,39 +10,40 @@ $(document).ready(function () {
   //Create code that adds in business hours (9am - 5pm) into file for adding in hourly events to the left of the 
   //event text box
 
-  //  moment().hour(i)
-  //  var hour = ('#9');
+  //    moment().hour(i)
+  //    var hour = parseInt($(this).attr('.hour'));
 
-  //for (var i = 9; i <= 17; i++) {
-  //    console.log(moment().hour(i).locale('en').format('hA'));
-  //    if ('#9' === '') {
-  //      $(hour).html(moment().hour(i).locale('en').format('hA'))
-  //    } else {
-  //      ''
+  //  for (var i = 9; i <= 10; i++) {
+  //      console.log(moment().hour(i).locale('en').format('hA'));
+  //        $('.hour').text(moment().hour(i).locale('en').format('hA'))
+
   //    }
-  //  }
 
   //Set styling so that business hours that have passed are greyed out;  if / else for time component 
   //Set styling so that the current time/hour is a red color;
   //Set styling so that hours that have not yet occured are in green 
-  var time = $('.time-block');
+
 
   function currentTime() {
     var current = moment().hours()
+    var time = $('.time-block');
 
-    console.log(current);
+    //    console.log(current);
 
     time.each(function () {
       var hour = parseInt($(this).attr('id'))
 
-      console.log(hour);
+    //  console.log(hour);
 
       if (hour === current) {
-        $('.time-block > textarea').attr('class', 'present col-sm-10')
+        $(this).children('.col-sm-10').attr('class', 'present col-sm-10 description')
+    //    console.log(('present' + hour + current))
       } else if (current > hour) {
-        $('.time-block > textarea').attr('class', 'past col-sm-10')
+        $(this).children('.col-sm-10').attr('class', 'past col-sm-10 description')
+    //    console.log(('past' + hour + current))
       } else {
-        $('.time-block > textarea').attr('class', 'future col-sm-10')
+        $(this).children('.col-sm-10').attr('class', 'future col-sm-10 description')
+    //    console.log(('future' + hour + current))
       }
     })
   }
@@ -48,21 +51,31 @@ $(document).ready(function () {
 
   //Set up local storage to store persistent changes in events by timeblock
 
-  var button = $('.saveBtn')
-
   button.on('click', function (event) {
     event.preventDefault();
     var text = $(this).siblings('.col-sm-10').val();
     var parent = $(this).parent().attr('id');
     localStorage.setItem(parent, JSON.stringify(text));
 
-    var toDo = (JSON.parse(localStorage.getItem(parent)));
-    $('#9 textarea').text(toDo);
-    console.log(parent);
-
   })
+
+  $('#9 textarea').val(localStorage.getItem('9'));
+  $('#10 textarea').val(localStorage.getItem('10'));
+  $('#11 textarea').val(localStorage.getItem('11'));
+  $('#12 textarea').val(localStorage.getItem('12'));
+  $('#13 textarea').val(localStorage.getItem('13'));
+  $('#14 textarea').val(localStorage.getItem('14'));
+  $('#15 textarea').val(localStorage.getItem('15'));
+  $('#16 textarea').val(localStorage.getItem('16'));
+  $('#17 textarea').val(localStorage.getItem('17'));
+//  console.log(parent);
+
   //Use styling to transform pointer into a disk icon at the time of click event (saving of event)
 
+ // button.on('click', function (event) {
+ //   $('selector').css('cursor', 'pointer')
+ // })
+  
 
 
 });
